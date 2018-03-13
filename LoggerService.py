c0a236345bb8dce215910fa5dca529cc4d5a5da3
MarkS <mark@s.com>
2018-03-13 17:23:50 +1100

    13/03/2018 Version 0.8 - Threaded File Watcher - Log Service

---
+++ LoggerService.py
@@ -0,0 +1,10 @@
+import datetime
+
+file_header = 'record'
+
+def WritetoLog(thread_id, message):
+    dt = datetime.datetime.now()
+    file_name = "{}_{}_{}_{}.log".format(file_header, str(dt.year), str(dt.month), str(dt.day))
+    with open(file_name, 'a') as f:
+        f.write("{}: {}\n".format(thread_id, message))
+    print("{}: {}\n".format(thread_id, message))
--- filemover.py
+++ filemover.py
@@ -1,6 +1,7 @@
 import os
 import re
 import threading
+from time import sleep
 
 # FOR WINDOWS TESTING
 s_dir = 'C:/Users/MarkS/PycharmProjects/plex_management/Test_folder'
@@ -15,18 +16,12 @@ valid = re.compile(r'[sS][0-9]+[eE][0-9]+', re.ASCII)
 
 def init_search_list():
     global dir_search_list
+    dir_search_list = []
     curr_dir = os.listdir(s_dir)
     for file in curr_dir:
         full_path = s_dir + '/' + str(file)
         if os.path.isdir(full_path):
             dir_search_list.append(full_path)
-
-class file_moving_thread(threading.Thread):
-    def __init__(self):
-        init_search_list()
-
-    def run(self):
-        pass
 
 def breakdownpath(spath):
     global valid
@@ -39,30 +34,56 @@ def breakdownpath(spath):
         print('NO MATCH')
     return sresult
 
+class file_moving_thread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        # init_search_list()
+        self.KeepRunning = True
+
+    def run(self):
+        global dir_search_list
+
+        while(self.KeepRunning):
+
+            sleep_timer = 15
+
+            init_search_list()
+
+            if dir_search_list:
+                # change sleep timer for more closer checks
+                sleep_timer = 2
+                for dir in dir_search_list:
+                    if not os.path.exists(dir):
+                        continue
+                    print("Checking {}".format(dir))
+                    new_list = os.listdir(dir)
+                    for file in new_list:
+                        full_path = dir + '/' + str(file)
+                        if os.path.isfile(full_path) and (file.endswith('mkv') or file.endswith('avi')):
+                            # create name for where the file is to be moved too
+                            result = breakdownpath(str(file))
+                            # check if it has a folder
+                            full_path_folder = s_tv_dir + '/' + result + '/'
+                            if not result in tv_dir_list:
+                                # Make Directory
+                                os.mkdir(full_path_folder)
+                            # Move file
+                            print('Moving: ' + full_path)
+                            print('To: ' + full_path_folder + str(file))
+                            os.rename(full_path, full_path_folder + str(file))
+                        elif os.path.isfile(full_path) and file.endswith('part'):
+                            # Not yet finished move on
+                            pass
+                        elif os.path.isfile(full_path):
+                            # Everything else is a file we don't want so delete it
+                            os.remove(full_path)
+                    if not os.listdir(dir):
+                        os.rmdir(dir)
+
+            print(sleep_timer)
+
+            sleep(sleep_timer * 60)
+
 if __name__ == "__main__":
-    init_search_list()
-    for dir in dir_search_list:
-        print("Checking {}".format(dir))
-        new_list = os.listdir(dir)
-        for file in new_list:
-            full_path = dir + '/' + str(file)
-            if os.path.isfile(full_path) and (file.endswith('mkv') or file.endswith('avi')):
-                # create name for where the file is to be moved too
-                result = breakdownpath(str(file))
-                # check if it has a folder
-                full_path_folder = s_tv_dir + '/' + result + '/'
-                if not result in tv_dir_list:
-                    # Make Directory
-                    os.mkdir(full_path_folder)
-                # Move file
-                print('Moving: ' + full_path)
-                print('To: ' + full_path_folder + str(file))
-                os.rename(full_path, full_path_folder + str(file))
-            elif os.path.isfile(full_path) and file.endswith('part'):
-                # Not yet finished move on
-                pass
-            elif os.path.isfile(full_path):
-                # Everything else is a file we don't want so delete it
-                os.remove(full_path)
-        if not os.listdir(dir):
-            os.rmdir(dir)
+    thread = file_moving_thread()
+    thread.start()
